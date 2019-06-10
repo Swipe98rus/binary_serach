cb3125235454081a204d7db0723c36849dba505b
Victor <vitya.ryabkov@gmail.com>
2019-06-10 22:54:02 +0300

    binarySearch complete

---
+++ binary_search.js
@@ -0,0 +1,37 @@
+
+let arrayNumber = [3,5,1,23,345,64374,347,412,956,46532,14,242,23,43,54,65,75,67,87,97];
+
+//Проводим сортировку массива
+const sorting = arrayNumber.sort((a,b)=>{return a-b});
+
+
+function binary_search(array, value) {
+    //Создадим нужные нам для поиска переменные
+     let high = array.length - 1;
+     let low = 0;
+     let mid = 0;
+
+
+
+     while( low <= high){
+         //ВЫчисляем середину массива
+         mid = Math.floor( (high+low)/2 );
+
+         //Условия поиска
+         if (array[mid] == value) {
+             return array[mid];
+         }else if( value > array[mid]){
+             low = mid + 1;
+         }else{
+             high = mid - 1;
+         }
+     };
+
+     //Значение не найдено
+     return -1;
+};
+
+
+
+let completeSearch = binary_search(sorting, 333);
+console.log(completeSearch)
